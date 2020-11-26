9ad83d188c4deba9dc7a891a1c219d42d1e965d0
steve <steve0639@cchntek.com>
2020-11-26 10:36:11 +0800

    [merge] version 0.1.0

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components/App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
+reportWebVitals();
