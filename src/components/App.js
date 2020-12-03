bae83cfefe69281ac66f997764a84d1f77a61aef
Jayne528 <38198968+Jayne528@users.noreply.github.com>
2020-12-04 00:31:22 +0800

    [update] test dnd func

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { Canvas } from './Canvas';
 import { loader } from './DataLoader';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 const GlobalStyle = createGlobalStyle`
   body { background:#1d2430; }
 `;
@@ -17,8 +20,22 @@ const StyledApp = styled.div`
     font-size:24px;
   }
 `;
+
+const StyledD = styled.div`
+  padding:16px;
+  color:#000;
+  font-size:16px;
+  background-color:#fff;
+  margin-bottom:5px;
+`;
+
 export const App = ()=> {
   const [ dataIsDone, setDataIsDone ] = useState(false);
+  const [ todo, setTodo ] = useState([
+    { oder:1, text:'dnd-1' },
+    { oder:2, text:'dnd-2' },
+    { oder:3, text:'dnd-3' }
+  ]);
   useEffect(()=>{
     loader.onComplete.add(()=>{
       setDataIsDone(true);
@@ -28,5 +45,44 @@ export const App = ()=> {
     <GlobalStyle/>
     {dataIsDone?<Canvas />:
       <span className="loading">Loading..</span>}
+    {/* DragDropContext */}
+    <DragDropContext 
+      onDragEnd={result => {
+        const { source, destination } = result;
+        if (!destination) {
+          console.log('over!')
+          return;
+        }
+        let arr = Array.from(todo);
+        const [remove] = arr.splice(source.index, 1);
+        arr.splice(destination.index, 0, remove);
+        setTodo(arr);
+        console.log(arr);
+      }}
+    >
+      <Droppable droppableId="id">
+        {provided => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {todo.map((val,key)=> (
+              <Draggable draggableId={`${key}`} index={key} key={key}>
+                {p=> (
+                  <StyledD 
+                    ref={p.innerRef}
+                    {...p.draggableProps}
+                    {...p.dragHandleProps}
+                    key={val.id}
+                  >{val.text}</StyledD>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+    {/* DragDropContext */}
   </StyledApp>
 };
